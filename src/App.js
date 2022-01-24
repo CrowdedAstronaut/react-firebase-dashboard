6c68c1dec3e7f3e9812efc253f703ad2a4ccceae
CrowdedAstronaut <ross@johnrossphillips.com>
2022-01-24 16:51:24 -0600

    Attempted to fix import statements

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./firebase/config";
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import React from "react";
-import Header from "./Header";
+// import Header from "./Header";
 import Signup from "./pages/Signup";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header></Header>
+      {/* <Header></Header> */}
       <div className="App">
         <div className="ui grid container">
           <Routes>
-            <Route
-              exact
-              path="/signup"
-              component={Signup}
-            />
+            <Route exact path="/signup" component={Signup} />
           </Routes>
         </div>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { logout } from "./firebase/auth";
+import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useSession } from "./firebase/UserProvider";
+import { useSession } from "react-use-session";
 
 function Header() {
   const navigate = useNavigate();
   const { user } = useSession();
 
   const logoutUser = async () => {
-    await logout();
+    await signOut();
     navigate.push("/login");
   };
 
@@ -16,10 +16,7 @@ function Header() {
     <header>
       <h2>The Grid</h2>
       {!!user && (
-        <button
-          className="ui secondary button logout"
-          onClick={logoutUser}
-        >
+        <button className="ui secondary button logout" onClick={logoutUser}>
           LOGOUT
         </button>
       )}
